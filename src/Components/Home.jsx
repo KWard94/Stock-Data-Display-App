5261c24e66f1c213def888ffecea0ebd8ce0122b
kward <pcward.k@gmail.com>
2021-01-13 15:00:36 -0700

    no api key, app broken, needs fetch and url

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-<<<<<<< HEAD
-import DummyCompA from './DummyCompA';
-=======
 // import DummyCompA from './DummyCompA';
->>>>>>> b00e1a1 (revert no API key)
 import Data from './Data';
 
 export default function Home () {
     return (
         <div className='home'>
             <div className='stockContainer'>
-<<<<<<< HEAD
-            <h1>HOME COMPONENT</h1>
-=======
             <h1>NY$E Company Data</h1>
->>>>>>> b00e1a1 (revert no API key)
             <Data />
 
 
