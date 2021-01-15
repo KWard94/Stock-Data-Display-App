3abec9e6081457b1d0e2c24faa436aef07a77b1c
kward <pcward.k@gmail.com>
2021-01-15 15:10:25 -0700

    styled detail view, fixed links in footer. next will be fixing the white space under footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { Router, Link } from 'react-router-dom';
 
 export default function Footer () {
     return(
@@ -13,11 +12,23 @@ export default function Footer () {
                 <div className='professional-links'>
 
                     <div className='linkedIn'>
-                    <a href='https://www.linkedin.com/in/keenan-k-ward/' target='_blank' rel='noreferrer'><img src='https://www.flaticon.com/svg/vstatic/svg/174/174857.svg?token=exp=1610644217~hmac=696969d2b330a3a4318e48dd14af9c56' alt='LinkedIn' height='10%'/></a>
+
+                    <a href='https://www.linkedin.com/in/keenan-k-ward/' target='_blank' rel='noreferrer'>
+                        LinkedIn <br/>
+                        <img src='https://www.flaticon.com/svg/vstatic/svg/174/174857.svg?token=exp=1610644217~hmac=696969d2b330a3a4318e48dd14af9c56' alt='LinkedIn' height='10%'/>
+
+                        </a>
+
                     </div>
 
                     <div className='git'>
-                    <a href='https://github.com/KWard94' target='_blank' rel='noreferrer'><img src='https://www.flaticon.com/svg/vstatic/svg/2111/2111425.svg?token=exp=1610644374~hmac=18d8fd1c56150cb152970af9b77b62e2' alt='GitHub' height='10%' /></a>
+
+                    <a href='https://github.com/KWard94' target='_blank' rel='noreferrer'>
+                        Github<br/>
+                        <img src='https://www.flaticon.com/svg/vstatic/svg/2111/2111425.svg?token=exp=1610644374~hmac=18d8fd1c56150cb152970af9b77b62e2' alt='GitHub' height='10%' />
+                        
+                        </a>
+
                     </div>
             
                 </div>
